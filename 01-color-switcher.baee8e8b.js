e00c2e08eafb75f566666817b6cc0ca8f51008f0
Iryna-Gry <108401183+Iryna-Gry@users.noreply.github.com>
2022-12-02 16:49:27 +0000

    Deploying to gh-pages from @ Iryna-Gry/goit-js-hw-09@00aee5c435eacce9bf21494c72dca8427e3d2a87 🚀

---
+++ 01-color-switcher.baee8e8b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.disabled=!0;let a=null;e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,a=setInterval((()=>{d.style=`background-color:#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.baee8e8b.js.map
