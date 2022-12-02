00aee5c435eacce9bf21494c72dca8427e3d2a87
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2022-12-02 18:48:46 +0200

    Udates task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+stopBtn.disabled = true;
+let intervalId = null;
+function updateBodyColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  intervalId = setInterval(() => {
+    body.style = `background-color:${getRandomHexColor()}`;
+  }, 1000);
+}
+startBtn.addEventListener('click', updateBodyColor);
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
